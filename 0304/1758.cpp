22e0fa87f93b44a8825eca790dd86b880a97da01
duswn137 <100579479+duswn137@users.noreply.github.com>
2022-03-10 20:44:36 +0900

    0304
    1758번

---
+++ 0304/1758.cpp
@@ -0,0 +1,27 @@
+//
+// Created by 고연주 on 2022/03/10.
+//
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    int ppl, tip;
+    int sum=0;
+    vector<int> arr;
+    cin>>ppl;
+    for (int i = 0; i < ppl; i++) {
+        cin>>tip;
+        arr.push_back(tip);
+    }
+    sort(arr.begin(), arr.end(), greater<int>());
+    for (int i = 0; i < ppl; i++) {
+        if(arr[i]-i<=0)
+            break;
+        sum+=(arr[i]-i);
+    }
+    cout<<sum;
+
+}
