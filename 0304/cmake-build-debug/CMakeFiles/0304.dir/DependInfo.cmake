167ffd5496edc83a0f8c8d41392bc6f27f2c4d1f
duswn137 <100579479+duswn137@users.noreply.github.com>
2022-03-11 10:32:01 +0900

    0304
    1026버ㄴ

---
+++ 0304/cmake-build-debug/CMakeFiles/0304.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/yj/Documents/GitHub/Altu_Bitu_YJ/0304/1026.cpp" "CMakeFiles/0304.dir/1026.cpp.o" "gcc" "CMakeFiles/0304.dir/1026.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ 0304/cmake-build-debug/CMakeFiles/0304.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "0304"
+  "0304.pdb"
+  "CMakeFiles/0304.dir/1026.cpp.o"
+  "CMakeFiles/0304.dir/1026.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/0304.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
