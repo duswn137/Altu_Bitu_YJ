a8d7b61f1e23313c659ba2bdd06a0a5d445b5718
duswn137 <100579479+duswn137@users.noreply.github.com>
2022-03-14 23:46:54 +0900

    0308
    20291번
    2776번

---
+++ 0308/2776.cpp
@@ -0,0 +1,35 @@
+//
+// Created by 고연주 on 2022/03/14.
+//
+
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+int main(){
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t, n, m;
+    cin>>t;
+    for (int i = 0; i < t; i++) {
+        cin>>n;
+        map<int, int> memo;
+        for (int j = 0; j < n; j++) {
+            int num;
+            cin>> num;
+            memo[num]=1;
+        }
+        cin>>m;
+        for (int j = 0; j < m; j++) {
+            int num;
+            cin >> num;
+            if(memo[num]==1)
+                cout<<"1"<<'\n';
+            else
+                cout<<"0"<<'\n';
+        }
+    }
+    return 0;
+}
