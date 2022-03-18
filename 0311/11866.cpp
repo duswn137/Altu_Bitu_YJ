3c33479494c119ca2ecbd96528171f32e4b29a45
duswn137 <100579479+duswn137@users.noreply.github.com>
2022-03-18 23:53:50 +0900

    0311
    11866
    2108

---
+++ 0311/11866.cpp
@@ -0,0 +1,32 @@
+//
+// Created by 고연주 on 2022/03/18.
+//
+
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+
+int main(){
+    int N,K;
+    queue<int> q;
+    cin>>N>>K;
+    for (int i=1; i<N+1; i++)
+        q.push(i);
+
+    cout<<"<";
+
+    while (!q.empty()) {
+        for (int i = 1; i < K; i++) {
+            q.push(q.front());
+            q.pop();
+        }
+        cout<<q.front();
+        q.pop();
+        if(!q.empty())
+            cout<<",";
+
+    }
+    cout<<">";
+}
---
+++ 0311/2108.cpp
@@ -0,0 +1,14 @@
+//
+// Created by 고연주 on 2022/03/17.
+//
+
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+
+}
